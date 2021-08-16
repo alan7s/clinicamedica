61c8888a177ca07c9dc8e335d884fabde73ba35b
FLS <alan7@outlook.com>
2021-08-16 13:04:12 -0300

    cadastro de planos

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin\{
     PlanController
 };
 
+Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
+Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
 Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
