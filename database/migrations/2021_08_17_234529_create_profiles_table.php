08af321982f0b467ed91ee4fd24c113769893999
FLS <alan7@outlook.com>
2021-08-18 10:24:31 -0300

    fix migration table profiles

--- database/migrations/2021_08_17_234529_create_profiles_table.php
+++ database/migrations/2021_08_17_234529_create_profiles_table.php
@@ -14,7 +14,7 @@ class CreateProfilesTable extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->bigIncrements('codigo');
+            $table->bigIncrements('id');
             $table->string('nome')->unique();
             $table->string('email')->unique();
             $table->string('telefone');
