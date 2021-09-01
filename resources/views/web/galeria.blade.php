33fcd16fd150c428a02c6b6502befd15c20957af
nandokfw <85894257+nandokfw@users.noreply.github.com>
2021-08-31 22:09:28 -0300

    Add files via upload

---
+++ resources/views/web/galeria.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Notícias sobre clínica médica, Galeria de imagens">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Galeira</title>
+    <link rel="stylesheet" href= "{{ url ('assets/css/style.css') }}" media="screen">
+	<link rel="stylesheet" href= "{{ url ('assets/css/galeria.css') }}" media="screen">
+    <script class="u-script" type="text/javascript" src="{{ url ('assets/js/jquery1.js') }}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{ url ('assets/js/style.js') }}" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "assets/images/cefet_1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Galeira">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body">
+  <header class="u-border-2 u-border-grey-75 u-clearfix u-header u-palette-1-light-1 u-header" id="sec-5350"><div class="u-clearfix u-sheet u-sheet-1">
+        <a class="u-image u-logo u-image-1">
+          <img src="assets/images/cefet_1.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1" data-responsive-from="MD">
+        
+          <div class="menu-collapse u-custom-font u-font-pt-sans" style="font-size: 1rem; letter-spacing: 0px; text-transform: uppercase; font-weight: 400;">
+            <a class="u-button-style u-custom-active-color u-custom-border u-custom-border-color u-custom-color u-custom-hover-color u-custom-left-right-menu-spacing u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-palette-1-base" href="#" style="background-image: none;">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>
+                  <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+                    </symbol>
+                        </defs></svg>
+            </a>
+          </div>
+          
+        
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-custom-font u-font-pt-sans u-nav u-spacing-2 u-unstyled u-nav-1">
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="home" style="padding: 10px 20px;">Home</a></li>
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="galeria" style="padding: 10px 20px;">Galeira</a>
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="login" style="padding: 10px 20px;">Login</a></li>
+
+                </li></ul>
+          </div>
+
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="web/home.blade.php" style="padding: 10px 20px;">Home</a></li>
+                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="web/galeria.blade.php" style="padding: 10px 20px;">Galeira</a>
+
+                </li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-0926">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">Notícias sobre clínica médica</h2>
+        <div class="u-carousel u-gallery u-gallery-slider u-layout-carousel u-lightbox u-no-transition u-show-text-on-hover u-gallery-1" id="carousel-d2d5" data-interval="5000" data-u-ride="carousel">
+          <ol class="u-absolute-hcenter u-carousel-indicators u-carousel-indicators-1">
+            <li data-u-target="#carousel-d2d5" data-u-slide-to="0" class="u-active u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
+            <li data-u-target="#carousel-d2d5" data-u-slide-to="1" class="u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
+            <li data-u-target="#carousel-d2d5" data-u-slide-to="2" class="u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
+            <li data-u-target="#carousel-d2d5" data-u-slide-to="3" class="u-grey-70 u-shape-circle" style="width: 10px; height: 10px;"></li>
+          </ol>
+          <div class="u-carousel-inner u-gallery-inner u-gallery-inner-1" role="listbox" data-image-width="316" data-image-height="160">
+            <div class="u-active u-carousel-item u-effect-fade u-gallery-item u-carousel-item-1" data-href="https://www.sanarmed.com/rotina-da-residencia-em-clinica-medica">
+              <div class="u-back-slide" data-image-width="674" data-image-height="379">
+                <img class="u-back-image u-expanded" src="assets/images/Rotina-da-residencia-em-Clinica-Medica-Sanar.jpg">
+              </div>
+              <div class="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-1">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-carousel-item u-effect-fade u-gallery-item u-carousel-item-2" data-image-width="1200" data-image-height="600" data-href="https://jomani.com.br/otimizar-os-processos-da-clinica-medica/">
+              <div class="u-back-slide">
+                <img class="u-back-image u-expanded" src="assets/images/estrategias-para-otimizar-os-processos-da-clinica-medica-1200x600.jpg">
+              </div>
+              <div class="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-2">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+              <style data-mode="XL"></style>
+              <style data-mode="LG"></style>
+              <style data-mode="MD"></style>
+              <style data-mode="SM"></style>
+              <style data-mode="XS"></style>
+            </div>
+            <div class="u-carousel-item u-effect-fade u-gallery-item u-carousel-item-3" data-image-width="1200" data-image-height="630" data-href="https://www.souenfermagem.com.br/cursos/100-horas/curso-enfermagem-em-clinica-medica-100h/">
+              <div class="u-back-slide">
+                <img class="u-back-image u-expanded" src="assets/images/65_2019-05-27_18-05-59_193_524410258.jpg">
+              </div>
+              <div class="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-3">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+              <style data-mode="XL"></style>
+              <style data-mode="LG"></style>
+              <style data-mode="MD"></style>
+              <style data-mode="SM"></style>
+              <style data-mode="XS"></style>
+            </div>
+            <div class="u-carousel-item u-effect-fade u-gallery-item u-carousel-item-4" data-href="https://asse.com.br/inadimplencia-em-clinicas-medicas-saiba-como-evitar/">
+              <div class="u-back-slide" data-image-width="316" data-image-height="160">
+                <img class="u-back-image u-expanded" src="assets/images/images.jpg">
+              </div>
+              <div class="u-align-center u-over-slide u-shading u-valign-bottom u-over-slide-4">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+          </div>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-1" href="#carousel-d2d5" role="button" data-u-slide="prev">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+            </span>
+            <span class="sr-only">
+              <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+            </span>
+          </a>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-grey-70 u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-carousel-control-2" href="#carousel-d2d5" role="button" data-u-slide="next">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+            </span>
+            <span class="sr-only">
+              <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+            </span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-section-2" id="sec-6f70">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">Galeria de imagens</h2>
+        <p class="u-text u-text-2">Aqui estão imagens relacionadas a nossa clínica.</p>
+        <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+          <div class="u-gallery-inner u-gallery-inner-1">
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide" data-image-width="600" data-image-height="401">
+                <img class="u-back-image u-expanded" src="assets/images/Lei-que-beneficia-clnicas-mdicas-e-hospitais--sancionada.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-1">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide" data-image-width="1149" data-image-height="768">
+                <img class="u-back-image u-expanded" src="assets/images/Tecnologias-clinicas-medicas-Hilab-1149x768.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-2">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide" data-image-width="512" data-image-height="341">
+                <img class="u-back-image u-expanded" src="assets/images/unnamed.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-3">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide" data-image-width="1024" data-image-height="535">
+                <img class="u-back-image u-expanded" src="assets/images/clinica-mdica-1024x535.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-4">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide">
+                <img class="u-back-image u-expanded" src="assets/images/Como-Montar-Uma-Clinica-Medica.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-5">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-gallery-item">
+              <div class="u-back-slide">
+                <img class="u-back-image u-expanded" src="assets/images/cefet_2.png">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-6">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-08cb"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Clínica médica CefetMed - Av. Amazonas, 5253 - Nova Suíça, Belo Horizonte - MG, 30421-169</p>
+        <a class="u-active-none u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-white u-btn-1"> (31) 3319-7000</a>
+      </div></footer>
+  </body>
+</html>
---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ url ('assets/css/style.css') }}" media="screen">
+<link rel="stylesheet" href="{{ url ('assets/css/Home.css') }}" media="screen">
+    <script class="u-script" type="text/javascript" src="{{ url ('assets/js/jquery1.js') }}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{ url ('assets/js/style.js') }}" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "assets/images/cefet_1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body"><header class="u-border-2 u-border-grey-75 u-clearfix u-header u-palette-1-light-1 u-header" id="sec-5350"><div class="u-clearfix u-sheet u-sheet-1">
+        <a class="u-image u-logo u-image-1">
+          <img src="assets/images/cefet_1.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1" data-responsive-from="MD">
+        
+          <div class="menu-collapse u-custom-font u-font-pt-sans" style="font-size: 1rem; letter-spacing: 0px; text-transform: uppercase; font-weight: 400;">
+            <a class="u-button-style u-custom-active-color u-custom-border u-custom-border-color u-custom-color u-custom-hover-color u-custom-left-right-menu-spacing u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-palette-1-base" href="#" style="background-image: none;">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>
+                  <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+                    </symbol>
+                        </defs></svg>
+            </a>
+          </div>
+          
+        
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-custom-font u-font-pt-sans u-nav u-spacing-2 u-unstyled u-nav-1">
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="home" style="padding: 10px 20px;">Home</a></li>
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="galeria" style="padding: 10px 20px;">Galeira</a>
+                <li class="u-nav-item"><a class="u-border-2 u-border-grey-75 u-button-style u-nav-link u-palette-1-base u-text-active-white u-text-hover-white u-text-white" href="login" style="padding: 10px 20px;">Login</a></li>
+
+                </li></ul>
+          </div>
+
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="web/home.blade.php" style="padding: 10px 20px;">Home</a></li>
+                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="web/galeria.blade.php" style="padding: 10px 20px;">Galeira</a>
+
+                </li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-center u-clearfix u-image u-shading u-section-1" src="" data-image-width="256" data-image-height="256" id="sec-aca6">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-default u-title u-text-1">Bem vindo ao CefeMed!</h1>
+        <p class="u-align-justify u-large-text u-text u-text-variant u-text-2">Muito bem vindo a nossa clínica Médica, CefetMed! O intuito da nossa clínica é realizar diagnósticos e tratamentos clínicos&nbsp; sem nenhuma cirurgia. Atendemos pacientes de todas as idades, avaliando-o de maneira mais eficaz possível e gerenciar seus cuidados de acordo com a especialidade necessária.</p>
+        <p class="u-align-justify u-text u-text-3">Nossa missão na clínica é resolver infermidades possíveis de serem resolvidas sem colocar o paciente em situações, como cirurgias, remédios mais fortes, etc, que não seriam necessárias nessas ocasições. O bem estar do paciente é nossa prioridade número um e por isso que nossa clínica possui médicos renomeados e conceituados em suas áreas.</p>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-2" id="sec-4048" src="assets/images/clinica-mdica-1024x535.jpg">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-image u-layout-cell u-size-30 u-image-1" data-image-width="1024" data-image-height="535">
+                <div class="u-container-layout u-container-layout-1"></div>
+              </div>
+              <div class="u-align-center u-container-style u-layout-cell u-size-30 u-layout-cell-2" >
+                <div class="u-container-layout u-container-layout-2">
+                  <h2 class="u-custom-font u-text u-text-default u-text-font u-text-1">
+                    <span style="font-style: italic;">Nossos valores</span>
+                    <span style="font-style: italic;"></span>
+                  </h2>
+                  <ul class="u-text u-text-default u-text-2">
+                    <li>Cuidar das vidas que passam pela clínica, sejam dos pacientes ou dos funcionários</li>
+                    <li>Transparência com todo tipo de procedimento e relações no trabalho</li>
+                    <li>Satisfação e bem estar do cliente está na nossa prioridade número um</li>
+                    <li>Qualidade em todo tipo de prestação de serviço realizada por nossos funcionários</li>
+                      <br>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-08cb"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Clínica médica CefetMed - Av. Amazonas, 5253 - Nova Suíça, Belo Horizonte - MG, 30421-169</p>
+        <a class="u-active-none u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-text-white u-btn-1"> (31) 3319-7000</a>
+      </div></footer>
+    -
+  </body>
+</html>
